6125e21e204549bb9e1d849fa54303d537e45fcf
fernandofm3 <fernandofm3@gmail.com>
2022-07-20 23:19:01 -0300

    Foi finalizado o menu mobile e também foi feita algumas partes do nanner.

---
+++ js/menu-mobile.js
@@ -0,0 +1,36 @@
+//Colocando ação ao botão hamburguer da nav no modo mobile
+const btnMobile = document.getElementById('btn-nav-mobile');
+
+function mostraMenuMobile (event) {
+
+    //Previne o comprtamento padrão (No mobile após o touch cancela o click)
+    if(event.type === 'touchstart') {
+        event.preventDefault();    
+    }
+
+    const navHeader = document.getElementById('nav-header'); 
+    navHeader.classList.toggle('active');
+
+    //Deixa mais acessivel informando que o botão vai expandir um conteúdo
+    const active = navHeader.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu.');
+
+        //Muda o icon de hamburguer para X(close)
+        document.getElementsByClassName("span-btn-open")[0].style.display = "none";
+        document.getElementsByClassName("span-btn-close")[0].style.display = "block";
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir menu.');        
+
+        //Muda o icon X(close) para hamburguer 
+        document.getElementsByClassName("span-btn-open")[0].style.display = "block";
+        document.getElementsByClassName("span-btn-close")[0].style.display = "none";
+    }
+}
+
+btnMobile.addEventListener('click', mostraMenuMobile);
+
+//Melhora a fluidez no mobobile
+btnMobile.addEventListener('touchstart', mostraMenuMobile);
